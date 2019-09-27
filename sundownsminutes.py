af9dbc43080a4e1bca31e2392c6b99f295425d85
Lee <moyolindelwe@gmail.com>
2019-09-27 07:03:03 +0200

    added Sun minutes

--- sundownsminutes.py
+++ sundownsminutes.py
@@ -2,16 +2,17 @@ import bs4
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-headers2 = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'}
-page2 = "https://www.transfermarkt.co.uk/mamelodi-sundowns-fc/leistungsdaten/verein/6356"
-pageTree2 = requests.get(page, headers=headers)
-pageSoup2 = BeautifulSoup(pageTree2.content, 'html.parser')
-Players2 = pageSoup2.find_all("a", {"class": "spielprofil_tooltip"})
-Values2 = pageSoup2.find_all("td", {"class": "rechts"})
-Players2List = []
-Values2List = []
+headers_sun = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) '
+                             'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'}
+page_sun = "https://www.transfermarkt.co.uk/mamelodi-sundowns-fc/leistungsdaten/verein/6356"
+pageTree_sun = requests.get(page_sun, headers=headers_sun)
+pageSoup_sun = BeautifulSoup(pageTree_sun.content, 'html.parser')
+Players_sun = pageSoup_sun.find_all("a", {"class": "spielprofil_tooltip"})
+Minutes_sun = pageSoup_sun.find_all("td", {"class": "rechts"})
+PlayersList_sun = []
+MinutesList_sun = []
 for i in range(0, 25):
-    PlayerList.append(Players[i].text)
-    MinutesList.append(Values[i].text)
-players_df = pd.DataFrame({"Players": PlayerList, "Values": MinutesList})
-players_df.head()
+    PlayersList_sun.append(Players_sun[i].text)
+    MinutesList_sun.append(Minutes_sun[i].text)
+players_df_sun = pd.DataFrame({"Players": PlayersList_sun, "Values": MinutesList_sun})
+players_df_sun.head()
